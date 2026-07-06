76aa9feece595c7eaa722115ed6d62fd3ea2ec91
ERROR429 <swayamvazatan90@gmail.com>
2026-07-06 12:28:43 +0530

    final

--- src/boot.s
+++ src/boot.s
@@ -1,42 +1,31 @@
 
 .code32
-
-# Declare constants for the Multiboot header
 .set ALIGN,    1<<0
 .set MEMINFO,  1<<1
 .set FLAGS,    ALIGN | MEMINFO
 .set MAGIC,    0x1BADB002
 .set CHECKSUM, -(MAGIC + FLAGS)
 
-# Multiboot header - Required by GRUB to recognize your kernel
 .section .multiboot, "a"
 .align 4
 .long MAGIC
 .long FLAGS
 .long CHECKSUM
 
-# Setup the stack
 .section .bss
 .align 16
 stack_bottom:
-.skip 16384 # 16 KiB
+.skip 16384 
 stack_top:
 
-# Entry point
 .section .text
 .global _start
-.extern kernel_main  # Correct placement
+.extern kernel_main  
 
 _start:
-    cli                 # <-- THE MISSING MUZZLE: Disable interrupts immediately!
-
-    # Initialize stack pointer
+    cli                 
     mov $stack_top, %esp
-
-    # Call your C kernel
     call kernel_main
-
-    # Safety: If kernel_main returns, trap the CPU
     cli
 .Lhang:
     hlt
